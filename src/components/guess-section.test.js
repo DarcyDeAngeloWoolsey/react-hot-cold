df47457ea5d35a15d90658745c5cc5d95b149b69
Darcy DeAngelo-Woolsey <Bucketlist2>
2017-11-06 09:29:56 -0500

    add test

---
+++ src/components/guess-section.test.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import renderer from 'react-test-renderer';
+
+import GuessSection from './guess-section';
+
+describe('<GuessSection />', () => {
+    it('Renders without crashing', () => {
+        shallow(<GuessSection />);
+                });
+
+        
+
+       
+
+    });
